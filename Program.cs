59d08d163dcc7afc6f5577223b891b3c52ae386b
Evgen <you@example.com>
2022-12-03 17:22:06 +0300

    о

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+Console.WriteLine("Введите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = a;
+int min =b;
+if (b>max) max = b;
+if (a<min) min = a;
+Console.WriteLine($"Максимальное число {max} ");
+Console.WriteLine($"Минимальное число {min} ");
+*/
+
+/*
+Console.WriteLine("Введите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число ");
+int d = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b>max) max = b;
+if (d>max) max = d;
+Console.WriteLine($"Максимальное число {max} ");
+*/
+
+
+
+/*
+Console.WriteLine("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+   if (a % 2 == 0)
+       Console.WriteLine($"Четное чило {a} ");
+   else
+       Console.WriteLine($"Нечетное чило {a} ");
+
+       */
+   
+/*
+Console.WriteLine("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 1;
+while (b < a){
+  b+=1;
+ 
+Console.WriteLine(b);
+}
+  Console.WriteLine($"Все положительные четные числа ");
+
+  */
+
+
+
+  
